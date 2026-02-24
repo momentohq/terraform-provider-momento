0c560b8c723de2ececd648dad2f3b3c155cf6d4f
anitarua <anita@momentohq.com>
2026-02-24 14:09:29 -0800

    chore: update docs and examples, show explicit dependency

--- docs/index.md
+++ docs/index.md
@@ -13,9 +13,19 @@ A terraform provider to manage [Momento](https://www.gomomento.com/) resources.
 ## Example Usage
 
 ```terraform
-# Configuration-based authentication
+# Select only one of the following configuration methods (or their environment variable equivalents).
+# Do not provide both legacy and non-legacy configurations.
+
+# Configuration-based authentication using legacy API key
 provider "momento" {
-  api_key = "my-token"
+  api_key = "my-legacy-momento-api-key"
+}
+
+# Configuration-based authentication using Momento API key and service hostname (using sdk endpoint format).
+# For more information: https://docs.momentohq.com/platform/regions#resp-and-sdk-endpoints
+provider "momento" {
+  v2_api_key      = "my-momento-api-key"
+  v2_api_endpoint = "cell-1-ap-southeast-1-1.prod.a.momentohq.com"
 }
 ```
 
---
+++ examples/resources/momento_object_store/resource.tf
@@ -0,0 +1,27 @@
+resource "momento_valkey_cluster" "example" {
+  cluster_name           = "cluster-name"
+  description            = "momento-managed valkey cluster"
+  enforce_shard_multi_az = false
+  node_instance_type     = "cache.t3.micro"
+  replication_factor     = 1
+  shard_count            = 1
+}
+
+# Creates a Momento object store in us-west-2 region with all optional configs (s3_prefix and access_logging_config) specified.
+# Waits for the valkey cluster to be created first if creating both for the first time.
+resource "momento_object_store" "example" {
+  name                = "object-store-name"
+  s3_bucket_name      = "s3-bucket-name"
+  s3_iam_role_arn     = "s3-iam-role-arn"
+  s3_prefix           = "prefix"
+  valkey_cluster_name = "cluster-name"
+  access_logging_config = {
+    iam_role_arn   = "cloudwatch-iam-role-arn"
+    log_group_name = "log-group-name"
+    region         = "us-west-2"
+  }
+  # Explicit dependency: Forces object store creation to wait for cluster creation
+  depends_on = [
+    momento_valkey_cluster.example,
+  ]
+}
